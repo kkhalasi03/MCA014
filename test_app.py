3b40840c04f977d6de48a34eea31e0a6fe543906
KRISHNA KHALASI <krishnakhalasi311@gmail.com>
2026-05-02 09:55:57 +0530

    Add test_app.py

---
+++ test_app.py
@@ -0,0 +1,21 @@
+import pytest
+from add import add,subtract,multiply,divide
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(5, 3) == 7
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(7, 5) == 3
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+    assert multiply(2, 5) == 10
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        calculator.divide(10, 0)
