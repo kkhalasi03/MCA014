12d093fc56c443137045f000acf41cbc2de2eb83
KRISHNA KHALASI <krishnakhalasi311@gmail.com>
2026-05-02 10:06:11 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,11 +3,11 @@ from app import add,subtract,multiply,divide
 
 def test_add():
     assert add(2, 3) == 5
-    assert add(5, 3) == 7
+    assert add(5, 3) == 8
 
 def test_subtract():
     assert subtract(5, 3) == 2
-    assert subtract(7, 5) == 3
+    assert subtract(7, 5) == 2
 
 def test_multiply():
     assert multiply(4, 3) == 12
